0179c9d2075f64a3383560e630a5649d7a1feed1
Abhyuday Mishra <88658148+12Mishra@users.noreply.github.com>
2026-02-23 21:13:14 +0530

    configured package.json along with prisma connection file

--- actions/db/db.js
+++ actions/db/db.js
@@ -1,7 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma= new PrismaClient({
-    log:["query"]
-})
+const globalForPrisma = globalThis;
 
-export default prisma;
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+}
